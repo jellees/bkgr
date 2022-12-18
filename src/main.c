1e08376f60fdc628f6dd822aa4bdea547c2e626a
Jelle <jelle.streekstra@hotmail.com>
2022-12-18 15:21:46 +0100

    Fix main.c a bit

--- src/main.c
+++ src/main.c
@@ -10,14 +10,22 @@
 #include "main.h"
 #include "common.h"
 
+/* --- Move this at some point */
 extern void sub_80001EC();
 extern void UpdateBackgrounds();
 extern u32 sub_80266A8();
+/* --- */
 
 static void UpdateGame(void);
-void nullsub_15();
-static void sub_800A528();
+static void sub_8009D2C();
+static void copy_sub_80001EC_to_iram();
+static void StartGame();
+static void prepare_wram();
+static void sub_800A344();
+static void nullsub_15();
 static void sub_800A37C();
+static void sub_800A528();
+static void sub_800A5F4();
 
 int AgbMain() {
     prepare_wram();
@@ -41,23 +49,31 @@ int AgbMain() {
 }
 
 static void UpdateGame(void) {
-    if (gIsPaletteEffectsActive)
+    if (gIsPaletteEffectsActive) {
         sub_8026DC0();
+    }
+
     call_functions();
     sub_80631AC();
     sub_800EACC();
     sub_800ECB4();
-    if (byte_200110C)
+
+    if (byte_200110C) {
         sub_800F430();
+    }
+
     sub_80344DC();
+
     if (!byte_203F9A1) {
         ReadKeys(&gKeysPressed, &gKeysDown, &gPreviousKeys);
-        if (gUnused_ExecUnusedInputFunc)
+        if (gUnused_ExecUnusedInputFunc) {
             Unused_InputFunction();
-    } else
+        }
+    } else {
         ReadKeysEx(&gKeysPressed, &gKeysDown, &gPreviousKeys);
+    }
 
-    _call_via_r0(gNullsub_3);
+    gNullsub_3();
     sub_806127C();
 
     if (word_203F998 == 12 && byte_20021F8 && !(gPlayerStateSettings[gPlayerState] & 0x800)) {
@@ -67,140 +83,170 @@ static void UpdateGame(void) {
             init_function(43);
         }
     }
-    if (dword_2001470)
+
+    if (dword_2001470) {
         sub_8015FD4();
+    }
 
     if (gKeysDown & 8 && !(gPlayerStateSettings[gPlayerState] & 0x800) && !byte_20021F0 && !byte_203F99C
         && gGameStatus.health && !gIsPaletteEffectsActive && !byte_203FA35) {
-        if ((gPlayerState != 101 || gIsSlideMiniGame)) {
-            if (!byte_2000F57) {
-                if (byte_20020BC) {
-                    sub_8016B0C();
-                    sub_804087C();
-                }
-                sub_80409DC();
-                if (dword_20020B8 != -1) {
-                    if (byte_203EA89)
-                        audio_halt_fx(dword_20020B8);
-                    dword_20020B8 = -1;
-                }
-                if (audio_fx_still_active(dword_20020B4) && byte_203EA89)
-                    audio_halt_fx(dword_20020B4);
-                if (gPlayerState == 35)
-                    sub_8064380();
-                open_pause_menu();
-                if (gPlayerState == 35)
-                    sub_806438C();
-                if (!gIsSlideMiniGame) {
-                    sub_8013DD4(21, 32);
-                    EnableBGAlphaBlending();
-                }
-                //_08009926
-                REG_BLDCNT = gColorSpecEffectsSel;
-                gKeysDown = 0;
-                gKeysPressed = 0;
-                if (gPlayerStateSettings[gPlayerState] & 0x100 && byte_20020B1 != 4) {
-                    sub_08040204(57, gGameStatus.field_16);
-                    sub_08041FA4(57);
-                }
-                if (gPlayerStateSettings[gPlayerState] & 4)
-                    sub_800DE04();
+        if ((gPlayerState != 101 || gIsSlideMiniGame) && !byte_2000F57) {
+            if (byte_20020BC) {
+                sub_8016B0C();
+                sub_804087C();
             }
-        } else
-            goto label_08009984;
+
+            sub_80409DC();
+
+            if (dword_20020B8 != -1) {
+                if (byte_203EA89) {
+                    audio_halt_fx(dword_20020B8);
+                }
+
+                dword_20020B8 = -1;
+            }
+
+            if (audio_fx_still_active(dword_20020B4) && byte_203EA89) {
+                audio_halt_fx(dword_20020B4);
+            }
+
+            if (gPlayerState == 35) {
+                sub_8064380();
+            }
+
+            open_pause_menu();
+
+            if (gPlayerState == 35) {
+                sub_806438C();
+            }
+
+            if (!gIsSlideMiniGame) {
+                sub_8013DD4(21, 32);
+                EnableBGAlphaBlending();
+            }
+
+            REG_BLDCNT = gColorSpecEffectsSel;
+            gKeysDown = 0;
+            gKeysPressed = 0;
+
+            if (gPlayerStateSettings[gPlayerState] & 0x100 && byte_20020B1 != 4) {
+                sub_08040204(57, gGameStatus.field_16);
+                sub_08041FA4(57);
+            }
+
+            if (gPlayerStateSettings[gPlayerState] & 4) {
+                sub_800DE04();
+            }
+        }
     }
-    //_0800997C
+
     if (!gIsSlideMiniGame) {
-    label_08009984:
         s_load_object(gCameraPixelX + 120, gCameraPixelY + 80);
         sub_800DF34();
         sub_8062484();
     }
-    //_080099A0
+
     if ((!byte_203F99C || byte_203F9A1) && !byte_203FA35) {
-        //_080099BC
         if (!(gKeysPressed & word_20010AC)) {
             word_20010AC = 0;
             if (!gIsSlideMiniGame) {
-                //_08009A60
-                if (!byte_200110C)
+                if (!byte_200110C) {
                     UpdatePlayerBehavior(gKeysPressed, gKeysDown);
+                }
             } else {
                 sub_80038A4(dword_2000FC8);
                 if ((gPlayerStateSettings[gPlayerState] & 0x800)
-                    || (gPlayerStateSettings[gPlayerState] & 0x1000))
+                    || (gPlayerStateSettings[gPlayerState] & 0x1000)) {
                     UpdatePlayerBehavior(gKeysPressed, gKeysDown);
+                }
             }
         }
-        //_08009A92
+
         if (!gIsSlideMiniGame) {
             sub_80038A4(dword_2000FC8);
             update_player();
         }
-    } else if (byte_203F99F) //_08009ABC
+    } else if (byte_203F99F) {
         UpdatePlayerBehavior(gKeysPressed, gKeysDown);
-    //_08009AD0
+    }
+
     dword_30032B4[0] = gCameraPixelX - 20;
     dword_30032B4[1] = gCameraPixelY - 5;
     dword_30032BC[0] = gCameraPixelX + 260;
     dword_30032BC[1] = gCameraPixelY + 170;
-    if (gPlayerState == 35)
+
+    if (gPlayerState == 35) {
         sub_8063B5C();
-    if (!gIsSlideMiniGame && gPlayerState != 101)
+    }
+
+    if (!gIsSlideMiniGame && gPlayerState != 101) {
         sub_8048C78();
-    //_08009B1A
+    }
+
     sub_804087C();
+
     if (!gIsSlideMiniGame) {
         s16 r1;
         u32 r0;
         u32* r2;
         u32 r4 = sub_800C50C();
+
         SetSpritePriority(&gPlayerSprite, r4);
-        if (Abs(gPlayerPos.y - gPlayerShadowPos.y) <= 0x4FFFF || dword_203DFC4)
-            SetSpritePriority(&gPlayerShadowSprite, r4); //_08009B50
-        else                                             //_08009B98
+
+        if (Abs(gPlayerPos.y - gPlayerShadowPos.y) <= 0x4FFFF || dword_203DFC4) {
+            SetSpritePriority(&gPlayerShadowSprite, r4);
+        } else {
             SetSpritePriority(&gPlayerShadowSprite, stru_3002950.playerSpritePriority);
-        //_08009BA2
+        }
+
         r2 = &gPlayerShadowSprite;
         r1 = (gPlayerPos.y - gPlayerShadowPos.y) >> 16;
-        if (r1 >= dword_80CEBC4)
+
+        if (r1 >= dword_80CEBC4) {
             r0 = byte_80CEB84[dword_80CEBC4 - 1];
-        else
+        } else {
             r0 = byte_80CEB84[r1];
+        }
+
         sub_80037F4(r2, r0);
     }
-    //_08009BE4
+
     DmaFill32(170, gOAMBuffer1, 0x100);
     gOAMBufferFramePtr = gOAMBuffer1;
     gOAMBufferEnd = &gOAMBuffer1[256];
     gOBJTileFramePtr = gOBJTileFrameStart;
     gOBJTileCount = dword_3003DA0;
+
     exec_arcade_menu();
     sub_8025278();
     show_room_name();
     sub_804095C();
+
     if (gPlayerStateSettings[gPlayerState] & 0x800) {
         if (byte_2000F5D) {
             sub_8025948(0);
-            if (gKeysDown & 1)
+
+            if (gKeysDown & 1) {
                 byte_2000F5D = FALSE;
-        } else //_08009C88
-        {
+            }
+        } else {
             if (byte_2002E4A) {
                 byte_2002E4A = FALSE;
                 byte_2000F5D = TRUE;
             }
+
             sub_8025948(gKeysPressed & 1);
         }
-    } else //_08009CAC
+    } else {
         RenderText();
-    //_08009CB0
+    }
+
     sub_8026234();
     sub_80408F0();
     sub_8061F80();
     sub_8064234();
     draw_arcade_menu();
-    _call_via_r0(gNullsub_4);
+    gNullsub_4();
 
     if (!gIsSlideMiniGame && gPlayerState != 101) {
         sub_8009D2C();
@@ -211,7 +257,7 @@ static void UpdateGame(void) {
     }
 }
 
-void sub_8009D2C() {
+static void sub_8009D2C() {
     dword_30032B0 = (u16*)gOAMBufferFramePtr;
     gOAMBufferFramePtr = gOAMBuffer2;
     gOAMBufferEnd = (u32*)((u16*)gOAMBufferFramePtr
@@ -239,7 +285,7 @@ void nullsub_4() {
 void nullsub_5() {
 }
 
-void prepare_wram() {
+static void prepare_wram() {
     u8 v0 = gShowEraseDataScreen;
     u8 v1 = byte_2000F59;
     u8 v2 = byte_2000F5A;
@@ -278,7 +324,7 @@ void EnableDisplay() {
     REG_BG3VOFS = 0;
 }
 
-void sub_8009ED8() {
+static void sub_8009ED8() {
     s32 i;
 
     dword_2000F68 = 0;
@@ -298,11 +344,11 @@ void sub_8009ED8() {
     REG_KEYCNT = 0xC00F;
 }
 
-void copy_sub_80001EC_to_iram() {
+static void copy_sub_80001EC_to_iram() {
     DmaTransfer32(sub_80001EC, &unk_3000000, ((UpdateBackgrounds - sub_80001EC) >> 2) + 1);
 }
 
-void StartGame() {
+static void StartGame() {
     gSpriteDMACount = 0;
     gTileAnimQueueIndex = 0;
     byte_2000314 = 0;
@@ -425,7 +471,7 @@ void StartGame() {
     }
 }
 
-void sub_800A344() {
+static void sub_800A344() {
     byte_2000330 = 0;
     byte_2000331 = 0;
     byte_2000332 = 0;
@@ -433,11 +479,6 @@ void sub_800A344() {
     byte_2000334 = 0;
     dword_200032C = 0;
 }
-
-// This should be defined in debug.c
-extern u8 byte_2001200;
-extern u8 gMainFrameCounter;
-extern u8 gDebugFPS;
 
 static void sub_800A37C() {
     ASSERT(dword_2000F6C == 0);
@@ -490,7 +531,7 @@ static void sub_800A37C() {
     }
 }
 
-void nullsub_15() {
+static void nullsub_15() {
 }
 
 void UpdateVideo() {
@@ -551,7 +592,7 @@ void sub_800A5E8() {
     sub_800A5F4();
 }
 
-void sub_800A5F4() {
+static void sub_800A5F4() {
     DmaFill32(0, byte_2000FCC, 30);
     DmaFill32(0, &gGameStatus, 8);
     dword_2001104 = 0;
