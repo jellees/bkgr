234c3ea62f2347c52db3627bf8da2063bddc7c9f
Jelle <jelle.streekstra@hotmail.com>
2023-01-16 20:21:42 +0100

    Add progress badge

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Banjo Kazooie Grunty's Revenge
 This is a decompilation of Banjo Kazooie Grunty's Revenge (US).
 
+![progress](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/jellees/b35087f821408554ad6d43f1ca9b1399/raw/bkgr-progress.json)
+
 It builds the following ROM:
 * bkgr.gba `sha1: 8f5b7c20e8be0f1e7919915b905c2b182a993cec`
--- score.py
+++ score.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys, re
 
 if __name__ == "__main__":
@@ -18,5 +19,9 @@ if __name__ == "__main__":
                     src += int(match[1], 0)
     
     total = src + asm
-    print("src: {:.2f}%, 0x{:x} bytes".format(100 * src / total, src))
-    print("asm: {:.2f}%, 0x{:x} bytes".format(100 * asm / total, asm))
+
+    if len(sys.argv) == 1:
+        print("src: {:.2f}%, 0x{:x} bytes".format(100 * src / total, src))
+        print("asm: {:.2f}%, 0x{:x} bytes".format(100 * asm / total, asm))
+    elif len(sys.argv) == 2 and sys.argv[1] == "-p":
+        print("{:.2f}".format(100 * src / total))
